96f48c62796300fa64d421289632d407ac5db9a4
ersanne <K1j@3QePIVFz>
2020-03-01 13:21:54 +0000

    Update PWA config with correct filename

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ module.exports = {
         msTileColor: '#0f0f0f',
         appleMobileWebAppCapable: 'yes',
         iconPaths: {
-            favicon32: 'img/icons/favicon-32x32.png',
-            favicon16: 'img/icons/favicon-16x16.png',
+            favicon32: 'img/icons/favicon_32x32.png',
+            favicon16: 'img/icons/favicon_16x16.png',
             appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
             msTileImage: 'img/icons/msapplication-icon-144x144.png'
         },
