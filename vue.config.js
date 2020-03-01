ebe4228a8129e8ce02e4c2343c677d285497fa22
ersanne <K1j@3QePIVFz>
2020-03-01 01:29:29 +0000

    Added pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    pwa: {
+        name: 'Erik Sanne',
+        themeColor: '#e5243f',
+        msTileColor: '#0f0f0f',
+        appleMobileWebAppCapable: 'yes',
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        },
+        start_url: '/',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'dev/sw.js',
+        }
+    }
 }
