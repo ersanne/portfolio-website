222c93be6deff3e8fab4d3ab00c6fa62b4f126b6
ersanne <erik@eriksanne.com>
2020-03-24 14:53:34 +0000

    Use correct method to toggle dark mode in bottom nav and optimize sizing

--- src/components/nav/BottomNav.vue
+++ src/components/nav/BottomNav.vue
@@ -14,17 +14,19 @@
                         :to="link.path"
                         class="subtitle-1 font-weight-medium"
                         exact-active-class="primary--text subtitle-1 font-weight-medium"
-                        text>
+                        text
+                        block>
                     {{ link.name }}
                 </v-btn>
             </v-col>
-            <v-col align-self="center">
+            <v-col cols="5" xs="4" sm="3" class="pl-3" align-self="center">
                 <v-switch
-                        v-model="$vuetify.theme.dark"
+                        v-model="$store.state.darkMode"
                         label="Dark Mode"
                         class="pt-1 mt-0"
                         hide-details
                         dense
+                        @click.stop.prevent="toggleTheme()"
                 ></v-switch>
             </v-col>
         </v-row>
@@ -38,7 +40,12 @@
         name: "BottomNav",
         computed: mapState([
             'navLinks'
-        ])
+        ]),
+        methods: {
+            toggleTheme() {
+                this.$store.dispatch('toggleDarkMode');
+            }
+        }
     }
 </script>
 
